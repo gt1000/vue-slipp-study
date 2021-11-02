e74a521cd7bc9cc06cfca6892516509565de0944
DESKTOP-H3MKUI2\Cheon JeongDae <gt1000@gmail.com>
2021-11-03 00:27:53 +0900

    인터셉터 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,15 @@
 import axios from "axios";
+import { setInterceptors } from "./common/interceptors";
 
-const instance = axios.create({
-    baseURL: process.env.VUE_APP_API_URL
-})
+function createInstance() {
+    const basicInstance = axios.create({
+        baseURL: process.env.VUE_APP_API_URL
+    })
+
+    return setInterceptors(basicInstance);
+}
+
+const instance = createInstance();
 
 function loginUser(userData) {
     return instance.post('login', userData);
